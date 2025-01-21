e78553e55a9e73e11e9a1b3b4c5aaa40be8604dc
AntonioMCGomes <marxmarc@gmail.com>
2025-01-21 19:14:15 +0000

    Menu

---
+++ menu/script.js
@@ -0,0 +1,7 @@
+function cliquei() {
+    if (menuList.style.display === 'block') {
+        menuList.style.display = 'none'
+    } else {
+        menuList.style.display = 'block'
+    }
+}
---
+++ script.js
@@ -0,0 +1,24 @@
+//alert('Olá, mundo!') Um comentário.
+
+
+/*
+    Selecina um elemento no DOM
+    e atualiza o valor desse elemento com uma string.
+*/
+//document.getElementById('inome').value = 'Olá, mundo!';
+
+
+//String
+var texto = 'Curso de JavaScript.'
+
+//Number
+
+var numeroInteiro = 789
+var numeroFracionado = 456.3
+
+//boolean
+
+var teste = false
+var estado = true 
+
+document.write(texto)
