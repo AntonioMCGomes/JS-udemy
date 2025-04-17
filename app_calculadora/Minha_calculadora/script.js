98e33b3a6feaadfe7af47e9b75e33a7652db7b0b
AntonioMCGomes <marxmarc@gmail.com>
2025-04-17 10:56:58 +0100

    calculadora update

--- app_calculadora/Minha_calculadora/script.js
+++ app_calculadora/Minha_calculadora/script.js
@@ -1,7 +1,7 @@
 
        
 function calcular(tipo, valor){
-    if (tipo === 'acao'){
+    if (tipo === 'acao'){ //primeira parte da estrutura lógica
         if (valor === 'c'){
             //limpar visor
             document.getElementById('isaida').value = ''
@@ -10,12 +10,13 @@ function calcular(tipo, valor){
             document.getElementById('isaida').value += valor
         }
         if (valor === '='){
-            let valorCampo = eval(document.getElementById('isaida').value)
+            let valorCampo = eval(document.getElementById('isaida').value) //valor recuperado
 
             //sobreposição do valor
             document.getElementById('isaida').value = valorCampo
         }
-    } else if (tipo === 'valor'){
+    } else if (tipo === 'valor'){ //segunda parte da estrutura lógica
+        
         document.getElementById('isaida').value += valor
     }
 }
